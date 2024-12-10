3bcdb7e677bd247f28cb8704a03756d0be5652c7
AnnetteScott <notnatural21@gmail.com>
2024-12-11 09:55:57 +1300

    Set up for day 11

---
+++ getInput.ts
@@ -0,0 +1,6 @@
+import { existsSync, readFileSync, writeFileSync } from 'node:fs';
+
+export function getPuzzleInput (day: number) {
+	const path = `./inputs/day${day.toString().padStart(2, '0')}.txt`;
+	return readFileSync(path, { encoding: 'utf-8' }).replaceAll('\r', '').trim();
+}
