a11fe630b62902d06093bd7bcf718731786a6012
AnnetteScott <notnatural21@gmail.com>
2024-12-14 22:32:42 +1300

    redo part 1

--- days/day12.ts
+++ days/day12.ts
@@ -1,63 +1,115 @@
 import { getPuzzleInput } from '../getInput';
 
-const input = getPuzzleInput(12, false).split('\n');
+const input = getPuzzleInput(12, true).split('\n').map(a => a.split(""));
 
-type Point = { x: number; y: number };
+class Point {
+	x: number;
+	y: number;
 
-function fencePrice(map: string[]) {
-	const height = map.length;
-	const width = map[0].length;
-	const visited: boolean[][] = Array.from({ length: height }, () => Array(width).fill(false));
-	let totalPrice = 0;
-
-	const directions = [
-		{ x: 0, y: 1 }, // right
-		{ x: 0, y: -1 }, // left
-		{ x: 1, y: 0 }, // down
-		{ x: -1, y: 0 }, // up
-	];
-
-	const isValidCell = (x: number, y: number): boolean => {
-		return x >= 0 && y >= 0 && x < height && y < width;
+	isInBounds(){
+		return this.x >= 0 && this.y >= 0 && this.x < width && this.y < height;
 	}
 
-	const dfs = (start: Point): { area: number; perimeter: number } => {
-		const plant = map[start.x][start.y];
-		const stack: Point[] = [start];
-		let area = 0;
-		let perimeter = 0;
-
-		while (stack.length > 0) {
-			const { x, y } = stack.pop()!;
-			if (visited[x][y]) continue;
-			visited[x][y] = true;
-			area++;
-
-			for (const direction of directions) {
-				const newX = x + direction.x;
-				const newY = y + direction.y;
-
-				if (!isValidCell(newX, newY) || map[newX][newY] !== plant) {
-					perimeter++;
-				} else if (!visited[newX][newY]) {
-					stack.push({ x: newX, y: newY });
-				}
-			}
-		}
-
-		return { area, perimeter };
-	};
-
-	for (let x = 0; x < height; x++) {
-		for (let y = 0; y < width; y++) {
-			if (!visited[x][y]) {
-				const { area, perimeter } = dfs({ x, y });
-				totalPrice += area * perimeter;
-			}
-		}
+	copy(){
+		return new Point(this.x, this.y);
 	}
 
-	return totalPrice;
+	constructor(x: number, y: number){
+		this.x = x;
+		this.y = y;
+	}
 }
 
-console.log("Part 1:", fencePrice(input));
+
+class Plant {
+	position: Point;
+	type: string;
+	
+	constructor(pos: Point, type: string){
+		this.position = pos;
+		this.type = type;
+	}
+}
+
+const height = input.length;
+const width = input[0].length;
+const garden: Plant[][] = [];
+const visited: boolean[][] = Array.from({ length: height }, () => Array(width).fill(false));
+
+function checkPointReq(checkPoint: Point, type: string){
+	if(!checkPoint.isInBounds()){
+		return;
+	}
+	
+	const visit = visited[checkPoint.y][checkPoint.x];
+	if(visit){
+		return;
+	}
+
+	if(input[checkPoint.y][checkPoint.x] === type){
+		const plant = new Plant(checkPoint, type);		
+		garden[garden.length - 1].push(plant);
+		visited[checkPoint.y][checkPoint.x] = true;
+		checkPointReq(new Point(checkPoint.x + 1, checkPoint.y), type);
+		checkPointReq(new Point(checkPoint.x - 1, checkPoint.y), type);
+		checkPointReq(new Point(checkPoint.x, checkPoint.y + 1), type);
+		checkPointReq(new Point(checkPoint.x, checkPoint.y - 1), type);
+	}
+}
+
+let prev = '';
+for(let i = 0; i < height; i++){
+	for(let j = 0; j < width; j++){
+		if(prev !== input[i][j]){
+			garden.push([])
+		}
+		checkPointReq(new Point(j, i), input[i][j]);
+		prev = input[i][j];
+	}
+}
+
+let tempI = 0;
+while(tempI < garden.length){
+	if(tempI < garden.length && garden[tempI].length === 0){
+		garden.splice(tempI, 1);
+	}else {
+		tempI++;
+	}
+
+}
+
+function getFencePrice(){
+	let price = 0;
+	for(const region of garden){
+		let perimeter = 0;
+		for(const plant of region){
+			const up = new Point(plant.position.x, plant.position.y - 1);
+			const down = new Point(plant.position.x, plant.position.y + 1);
+			const left = new Point(plant.position.x - 1, plant.position.y);
+			const right = new Point(plant.position.x + 1, plant.position.y);
+
+			if(!up.isInBounds() || input[up.y][up.x] !== plant.type){
+				perimeter++;
+			}
+			if(!down.isInBounds() || input[down.y][down.x] !== plant.type){
+				perimeter++;
+			}
+			if(!left.isInBounds() || input[left.y][left.x] !== plant.type){
+				perimeter++;
+			}
+			if(!right.isInBounds() || input[right.y][right.x] !== plant.type){
+				perimeter++;
+			}
+		}
+		price += region.length * perimeter;
+	}
+
+	return price;
+}
+
+
+console.log("Part 1:", getFencePrice())
+console.log(garden)
+}
+
+getSides()
