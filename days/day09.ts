536a8679fcf28ecf9e504778a74a5d7d8624b7d3
AnnetteScott <notnatural21@gmail.com>
2024-12-15 10:40:52 +1300

    day 9 part 1

--- days/day09.ts
+++ days/day09.ts
@@ -1,3 +1,43 @@
 import { getPuzzleInput } from '../getInput';
 
-const input = getPuzzleInput(9, true).split('\n');
+const stack = getPuzzleInput(9, false).split('').map(Number);
+const disk: string[] = [];
+
+let id = 0;
+while(stack.length > 0){
+	const files = stack.shift() ?? 0;
+	const freeSpace = stack.shift() ?? 0;
+
+	for(let i = 0; i < files; i++){
+		disk.push(id.toString());
+	}
+
+	disk.push(...'.'.repeat(freeSpace));
+	id++;
+}
+
+function movePartOne(disk: string[]){
+	for(let i = disk.length - 1; i > 0; i--){
+		const freeSpace = disk.indexOf('.')
+		const file = disk[i];
+		if(freeSpace <= i && file !== '.'){
+			disk[freeSpace] = file;
+			disk[i] = '.'
+		}
+	}
+	return disk;
+}
+
+function checkSum(disk: string[]){
+	let sum = 0;
+	for(let i = 0; i < disk.length; i++){
+		const file = disk[i];
+		if(file !== '.'){
+			sum += i * parseInt(file);
+		}
+	}
+	return sum;
+}
+
+console.log("Part 1:", checkSum(movePartOne(JSON.parse(JSON.stringify(disk)))))
+
