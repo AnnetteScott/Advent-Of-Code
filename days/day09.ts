227e019921e5de90b32e3746b145befdb4015cbd
AnnetteScott <notnatural21@gmail.com>
2024-12-15 11:27:40 +1300

    day 9 part 2

--- days/day09.ts
+++ days/day09.ts
@@ -4,9 +4,10 @@ const stack = getPuzzleInput(9, false).split('').map(Number);
 const disk: string[] = [];
 
 let id = 0;
-while(stack.length > 0){
-	const files = stack.shift() ?? 0;
-	const freeSpace = stack.shift() ?? 0;
+let stackCopy = [...stack];
+while(stackCopy.length > 0){
+	const files = stackCopy.shift() ?? 0;
+	const freeSpace = stackCopy.shift() ?? 0;
 
 	for(let i = 0; i < files; i++){
 		disk.push(id.toString());
@@ -41,3 +42,29 @@ function checkSum(disk: string[]){
 
 console.log("Part 1:", checkSum(movePartOne(JSON.parse(JSON.stringify(disk)))))
 
+function movePartTwo(disk: string[]){
+	let ignoreList: number[] = [];
+	for(let i = disk.length - 1; i > 0; i--){
+		const file = disk[i];
+
+		if(ignoreList.includes(parseInt(file)) || file === '.'){
+			continue;
+		}
+		const startI = disk.indexOf(file);
+		const blockLength = i - startI + 1;
+		const freeSpace = disk.map(a => a !== '.' ? '#' : '.').join('').indexOf('.'.repeat(blockLength));
+		if(freeSpace >= 0 && freeSpace < startI){
+			for(let j = freeSpace; j < blockLength + freeSpace; j++){
+				disk[j] = file;
+			}
+			for(let j = startI; j < blockLength + startI; j++){
+				disk[j] = '.';
+			}
+		}
+		ignoreList.push(parseInt(file));
+	}
+	return disk;
+}
+
+const result = movePartTwo(JSON.parse(JSON.stringify(disk)));
+console.log("Part 2:", checkSum(result))
