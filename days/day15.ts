0b379727c29ed424d1f89438961e897b7e17d348
AnnetteScott <notnatural21@gmail.com>
2024-12-16 10:23:57 +1300

    update part 1 movement

--- days/day15.ts
+++ days/day15.ts
@@ -17,18 +17,18 @@ class Robot {
 
 type Instructions = "^" | ">" | "<" | "v";
 
-const input = getPuzzleInput(15, false).split('\n\n').map(a => a.split("\n"));
+const input = getPuzzleInput(15, true).split('\n\n').map(a => a.split("\n"));
 const maze = input[0].map(a => a.split(''));
 const instructions = input[1].map(a => a.split('')) as Instructions[][];
 const height = maze.length;
 const width = maze[0].length;
 
 const robot = new Robot();
-getRobot();
+getRobot(maze);
 
-function getRobot(){
-	for(let i = 0; i < height; i++){
-		for(let j = 0; j < width; j++){
+function getRobot(maze: string[][]){
+	for(let i = 0; i < maze.length; i++){
+		for(let j = 0; j < maze[0].length; j++){
 			if(maze[i][j] === '@'){
 				robot.position = new Point(j, i);
 				return;
@@ -36,117 +36,70 @@ function getRobot(){
 		}
 	}
 }
+const directions = {
+	'<': {dx: -1, dy: 0},
+	'>': {dx: 1, dy: 0},
+	'^': {dx: 0, dy: -1},
+	'v': {dx: 0, dy: 1},
+} as {[key in Instructions]: {dx: number, dy: number}}
 
-function move(instruction: Instructions){
-	if(instruction === '<'){
+function partOne(maze: string[][]){
+	function move(instruction: Instructions){
+		const {dx, dy} = directions[instruction]
 		let boxLength = 0;
-		let index = robot.position.x - 1;
-		while(index > 0 && maze[robot.position.y][index] === 'O'){
+		let indexY = robot.position.y + dy;
+		let indexX = robot.position.x + dx;
+
+		while(indexX > 0 && indexX < width &&
+			indexY > 0 && indexY < height &&
+			maze[indexY][indexX] === 'O'
+		){
 			boxLength++;
-			index--;
+			indexX += dx;
+			indexY += dy;
 		}
 
-		const freeSpace = maze[robot.position.y][robot.position.x - boxLength - 1] === '.'
+		const freeSpace = maze[robot.position.y + dy + (boxLength * dy)][robot.position.x + dx + (boxLength * dx)] === '.'
+		
 		if(boxLength > 0 && freeSpace){
-			for(let i = robot.position.x - 1; i >= robot.position.x - boxLength; i--){
-				maze[robot.position.y][i - 1] = 'O'
+			for(let i = 1; i <= boxLength; i++){
+				const boxX = robot.position.x + (dx * i) + dx;
+				const boxY = robot.position.y + (dy * i) + dy;
+				maze[boxY][boxX] = 'O'
 			}
-			maze[robot.position.y][robot.position.x - 1] = "@"
-			maze[robot.position.y][robot.position.x] = "."
-			robot.position.x -= 1;
-		}else if(maze[robot.position.y][robot.position.x - 1] === '.'){
-			maze[robot.position.y][robot.position.x - 1] = "@"
-			maze[robot.position.y][robot.position.x] = "."
-			robot.position.x -= 1;
-		}
-	}
-	else if(instruction === '>'){
-		let boxLength = 0;
-		let index = robot.position.x + 1;
-		while(index < width && maze[robot.position.y][index] === 'O'){
-			boxLength++;
-			index++;
 		}
 
-		const freeSpace = maze[robot.position.y][robot.position.x + boxLength + 1] === '.'
-		if(boxLength > 0 && freeSpace && maze[robot.position.y][robot.position.x + boxLength] !== '#'){
-			for(let i = robot.position.x + 1; i <= robot.position.x + boxLength; i++){
-				maze[robot.position.y][i + 1] = 'O'
-			}
-			maze[robot.position.y][robot.position.x + 1] = "@"
+		if((maze[robot.position.y + dy][robot.position.x + dx] === "O" && freeSpace) || 
+			maze[robot.position.y + dy][robot.position.x + dx] === "."
+		){
+			maze[robot.position.y + dy][robot.position.x + dx] = "@"
 			maze[robot.position.y][robot.position.x] = "."
-			robot.position.x += 1
-		}else if(maze[robot.position.y][robot.position.x + 1] === '.'){
-			maze[robot.position.y][robot.position.x + 1] = "@"
-			maze[robot.position.y][robot.position.x] = "."
-			robot.position.x += 1
+			robot.position.x += dx;
+			robot.position.y += dy;
 		}
 	}
-	else if(instruction === '^'){
-		let boxLength = 0;
-		let index = robot.position.y - 1;
-		while(index > 0 && maze[index][robot.position.x] === 'O'){
-			boxLength++;
-			index--;
-		}
 
-		const freeSpace = maze[robot.position.y - boxLength - 1][robot.position.x] === '.'
-		if(boxLength > 0 && freeSpace){
-			for(let i = robot.position.y - 1; i >= robot.position.y - boxLength; i--){
-				maze[i - 1][robot.position.x] = 'O'
-			}
-			maze[robot.position.y - 1][robot.position.x] = "@"
-			maze[robot.position.y][robot.position.x] = "."
-			robot.position.y -= 1
-		}else if(maze[robot.position.y - 1][robot.position.x] === '.'){
-			maze[robot.position.y - 1][robot.position.x] = "@"
-			maze[robot.position.y][robot.position.x] = "."
-			robot.position.y -= 1
+	for(const line of instructions){
+		for(const instr of line){
+			move(instr);
 		}
 	}
-	else if(instruction === 'v'){
-		let boxLength = 0;
-		let index = robot.position.y + 1;
-		while(index < height && maze[index][robot.position.x] === 'O'){
-			boxLength++;
-			index++;
-		}
-
-		const freeSpace = maze[robot.position.y + boxLength + 1][robot.position.x] === '.'
-		if(boxLength > 0 && freeSpace){
-			for(let i = robot.position.y + 1; i <= robot.position.y + boxLength; i++){
-				maze[i + 1][robot.position.x] = 'O'
+	
+	function checkSum(){
+		let sum = 0;
+		for(let i = 0; i < height; i++){
+			for(let j = 0; j < width; j++){
+				if(maze[i][j] === 'O'){
+					sum += (i * 100) + j;
+				}
 			}
-			maze[robot.position.y + 1][robot.position.x] = "@"
-			maze[robot.position.y][robot.position.x] = "."
-			robot.position.y += 1
-		}else if(maze[robot.position.y + 1][robot.position.x] === '.'){
-			maze[robot.position.y + 1][robot.position.x] = "@"
-			maze[robot.position.y][robot.position.x] = "."
-			robot.position.y += 1
 		}
+		return sum;
 	}
+	
+	console.log(maze.map(a => a.join('')).join("\n"))
+	console.log("Part 1:", checkSum());
 }
 
-for(const line of instructions){
-	for(const instr of line){
-		move(instr);
-	}
-}
-console.log(maze.map(a => a.join('')).join("\n"))
+partOne(JSON.parse(JSON.stringify(maze)))
 
-function checkSum(){
-	let sum = 0;
-	for(let i = 0; i < height; i++){
-		for(let j = 0; j < width; j++){
-			if(maze[i][j] === 'O'){
-				sum += (i * 100) + j;
-			}
-		}
-	}
-	return sum;
-}
-
-
-console.log("Part 1:", checkSum());
-console.log("Part 2:");
