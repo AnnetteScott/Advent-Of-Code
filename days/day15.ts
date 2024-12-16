322a6ecc44bcb269afd9d6fb3a409bbd2598f8cb
AnnetteScott <notnatural21@gmail.com>
2024-12-16 14:09:59 +1300

    day 15 part 2

--- days/day15.ts
+++ days/day15.ts
@@ -1,8 +1,21 @@
 import { getPuzzleInput } from '../getInput';
+import * as fs from "fs";
 
 class Point {
 	x: number;
 	y: number;
+
+	isInBounds(){
+		return this.x >= 0 && this.y >= 0 && this.x < width && this.y < height;
+	}
+
+	copy(){
+		return new Point(this.x, this.y);
+	}
+
+	isEqual(point: Point){
+		return point.x === this.x && point.y === this.y;
+	}
 
 	constructor(x: number, y: number){
 		this.x = x;
@@ -15,9 +28,19 @@ class Robot {
 	symbol = "@";
 }
 
+class Box {
+	pointOne: Point;
+	pointTwo: Point;
+
+	constructor(pointOne: Point, pointTwo: Point){
+		this.pointOne = pointOne;
+		this.pointTwo = pointTwo;
+	}
+}
+
 type Instructions = "^" | ">" | "<" | "v";
 
-const input = getPuzzleInput(15, true).split('\n\n').map(a => a.split("\n"));
+const input = getPuzzleInput(15, false).split('\n\n').map(a => a.split("\n"));
 const maze = input[0].map(a => a.split(''));
 const instructions = input[1].map(a => a.split('')) as Instructions[][];
 const height = maze.length;
@@ -97,9 +120,159 @@ function partOne(maze: string[][]){
 		return sum;
 	}
 	
-	console.log(maze.map(a => a.join('')).join("\n"))
+	/* console.log(maze.map(a => a.join('')).join("\n")) */
 	console.log("Part 1:", checkSum());
 }
 
 partOne(JSON.parse(JSON.stringify(maze)))
 
+function scaleUp(){
+	const newMap: string[][] = Array.from({ length: height }, () => []);
+	for(let i = 0; i < height; i++){
+		for(let j = 0; j < width; j++){
+			if(maze[i][j] === '#'){
+				newMap[i].push("#");
+				newMap[i].push("#");
+			}
+			else if(maze[i][j] === 'O'){
+				newMap[i].push("[");
+				newMap[i].push("]");
+			}
+			else if(maze[i][j] === '.'){
+				newMap[i].push(".");
+				newMap[i].push(".");
+			}
+			else if(maze[i][j] === '@'){
+				newMap[i].push("@");
+				newMap[i].push(".");
+			}
+		}
+	}
+	return newMap;
+}
+
+const scaleMaze = scaleUp()
+getRobot(scaleMaze);
+let output = "";
+
+function partTwo(){
+	const width = scaleMaze[0].length;
+	function move(instruction: Instructions){
+		const boxes = getBoxes(instruction);
+		const {dx, dy} = directions[instruction];
+		let freeSpace = false;
+
+		if(dx !== 0){
+			const length = boxes.length * 2;
+			freeSpace = scaleMaze[robot.position.y + dy + (length * dy)][robot.position.x + dx + (length * dx)] === '.'
+			
+			if(length > 0 && freeSpace){
+				for(const box of boxes){
+					scaleMaze[box.pointOne.y][box.pointOne.x + dx] = "["
+					scaleMaze[box.pointTwo.y][box.pointTwo.x + dx] = "]"
+				}
+			}
+		}else {
+			freeSpace = true;
+			for(const box of boxes){
+				if(scaleMaze[box.pointOne.y + dy][box.pointOne.x] === "#" || 
+					scaleMaze[box.pointTwo.y + dy][box.pointTwo.x] === "#"
+				){
+					freeSpace = false;
+					break;
+				}
+			}
+
+			if(freeSpace){
+				for(const box of boxes.sort((a, b) => dy < 0 ? a.pointOne.y - b.pointOne.y : b.pointOne.y - a.pointOne.y)){
+					scaleMaze[box.pointOne.y + dy][box.pointOne.x] = "["
+					scaleMaze[box.pointTwo.y + dy][box.pointTwo.x] = "]"
+					scaleMaze[box.pointOne.y][box.pointOne.x] = "."
+					scaleMaze[box.pointTwo.y][box.pointTwo.x] = "."
+				}
+			}
+		}
+		
+
+		if((scaleMaze[robot.position.y + dy][robot.position.x + dx] === "[" && freeSpace) || 
+			(scaleMaze[robot.position.y + dy][robot.position.x + dx] === "]" && freeSpace) || 
+			scaleMaze[robot.position.y + dy][robot.position.x + dx] === "."
+		){
+			scaleMaze[robot.position.y + dy][robot.position.x + dx] = "@"
+			scaleMaze[robot.position.y][robot.position.x] = "."
+			robot.position.x += dx;
+			robot.position.y += dy;
+		}
+	}
+
+	function getBoxes(instr: Instructions){
+		const boxes: Box[] = []
+		const {dx, dy} = directions[instr]
+		let indexY = robot.position.y + dy;
+		let indexX = robot.position.x + dx;
+		if(dx !== 0 && dy === 0){
+	
+			while(indexX > 0 && indexX < width &&
+				indexY > 0 && indexY < height &&
+				(
+					(scaleMaze[indexY][indexX] === ']' && scaleMaze[indexY][indexX + dx] === '[') ||
+					(scaleMaze[indexY][indexX] === '[' && scaleMaze[indexY][indexX + dx] === ']')
+				)
+			){
+				if(scaleMaze[indexY][indexX] === ']'){
+					boxes.push(new Box(new Point(indexX + dx, indexY), new Point(indexX, indexY)))
+				}else {
+					boxes.push(new Box(new Point(indexX, indexY), new Point(indexX + dx, indexY)))
+				}
+				indexX += (dx * 2);
+				indexY += (dy * 2);
+			}
+		}else if(dy !== 0 && dx == 0){
+			function dfsBoxes(point: Point){
+				if(point.x < 0 || point.x > width || point.y < 0 || point.y > height){
+					return;
+				}
+
+				const box = new Box(new Point(point.x, point.y), new Point(point.x + 1, point.y));
+				if(scaleMaze[point.y][point.x] === '[' && boxes.filter(a => a.pointOne.isEqual(box.pointOne)).length === 0){
+					boxes.push(box);
+					dfsBoxes(new Point(point.x, point.y + dy))
+					dfsBoxes(new Point(point.x + 1, point.y + dy))
+				}
+				else if(scaleMaze[point.y][point.x] === ']'){
+					dfsBoxes(new Point(point.x - 1, point.y))
+				}
+			}
+			dfsBoxes(new Point(indexX, indexY));
+		}
+		return boxes;
+	}
+
+	for(const line of instructions){
+		for(const instr of line){
+			move(instr);
+			output += scaleMaze.map(a => a.join('')).join("\n");
+			output += "\n\n";
+		}
+	}
+	
+	function checkSum(){
+		let sum = 0;
+		for(let i = 0; i < height; i++){
+			for(let j = 0; j < width; j++){
+				if(scaleMaze[i][j] === '['){
+					sum += (i * 100) + j;
+				}
+			}
+		}
+		return sum;
+	}
+	
+	//console.log(scaleMaze.map(a => a.join('')).join("\n"));
+	console.log("Part 2:", checkSum());
+}
+
+partTwo();
+fs.writeFileSync(`../day15.txt`, output, "utf8");
+// ??
+//1417290
