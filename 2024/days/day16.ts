d997eb46a0a867c7ec49aadb830b8e799ec7985c
AnnetteScott <notnatural21@gmail.com>
2024-12-17 10:57:07 +1300

    day 16 part 1

--- 2024/days/day16.ts
+++ 2024/days/day16.ts
@@ -1,3 +1,24 @@
 import { getPuzzleInput } from '../getInput';
-import { Point, print2d, printToFile, directions } from '../../common'
-const input = getPuzzleInput(16, true).split('\n');
+import { Point, dijkstraRotation } from '../../common'
+const maze = getPuzzleInput(16, false).split('\n').map(a => a.split(""));
+const height = maze.length;
+const width = maze[0].length;
+
+const start = new Point(0, 0, 'S', width, height);
+const end = new Point(0, 0, 'E', width, height);
+
+for(let i = 0; i < height; i++){
+	for(let j = 0; j < width; j++){
+		if(maze[i][j] === 'S'){
+			start.x = j;
+			start.y = i;
+		}
+		if(maze[i][j] === 'E'){
+			end.x = j;
+			end.y = i;
+		}
+	}
+}
+
+const cost = dijkstraRotation(maze, start, end);
+console.log(cost)
