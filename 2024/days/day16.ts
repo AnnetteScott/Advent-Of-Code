23883955bcf9002f7dde32c68a373f58c88b2cbd
AnnetteScott <notnatural21@gmail.com>
2024-12-16 16:06:15 +1300

    move 2024 files to own folder

--- 2024/days/day16.ts
+++ 2024/days/day16.ts
@@ -1,3 +1,3 @@
 import { getPuzzleInput } from '../getInput';
-
+import {type Point, print2d, printToFile } from '../../common'
 const input = getPuzzleInput(16, true).split('\n');
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import * as path from "path";
 
-for (let i = 12; i <= 25; i++) {
+for (let i = 1; i <= 25; i++) {
 	const folderName = i.toString().padStart(2, "0"); // Formats as "01", "02", etc.
 	fs.writeFileSync(`./days/day${folderName}.ts`, `import { getPuzzleInput } from '../getInput';\n\nconst input = getPuzzleInput(${i}, true).split('\\n');`, "utf8");
 }
